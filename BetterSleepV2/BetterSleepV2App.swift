e754f19a03d79532e424aac96c1d76e12666ff53
Lexter Tapawan <lexter.tapawan@gmail.com>
2024-01-25 10:46:21 +0000

    Initial Commit

---
+++ BetterSleepV2/BetterSleepV2App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepV2App.swift
+//  BetterSleepV2
+//
+//  Created by Lexter Tapawan on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterSleepV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterSleepV2
+//
+//  Created by Lexter Tapawan on 25/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
